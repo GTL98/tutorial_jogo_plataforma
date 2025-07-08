2f8ad096937556bd39936b89ac31c666362969f4
Guilherme Trevisan Linhares <gui.pythonprojetos@gmail.com>
2025-07-07 22:56:04 -0300

    Aula 01

---
+++ Aula 01 - Configuração do level/codigo/sprites.py
@@ -0,0 +1,21 @@
+# --- Importar o Pygame --- #
+import pygame
+
+# --- Importar as configurações --- #
+from configuracoes import *
+
+
+# --- Classe dos sprites --- #
+class Sprites(pygame.sprite.Sprite):
+    """Classe para a criação dos sprites."""
+    def __init__(self, posicao, superficie, grupos):
+        """Função responsável por inicializar a classe."""
+        # --- Herdar a classe Sprite() --- #
+        super().__init__(grupos)
+
+        # --- Imagem do sprite --- #
+        self.image = pygame.Surface((TAMANHO_TILE, TAMANHO_TILE))
+        self.image.fill('white')
+
+        # --- Retângulo do sprite --- #
+        self.rect = self.image.get_frect(topleft=posicao)
